fb171a0d4507f225fed2fa71ec436d71c1688b78
foli05 <85459781+foli05@users.noreply.github.com>
2021-06-21 13:39:43 -0500

    Initial commit

---
+++ UWPSoundboard/Model(s)/Sound.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundboard.Model_s_
+{
+    public enum Soundcategory
+    { Animals,
+      Cartoons,
+      Taunts,
+      Warnings}
+        public class Sound
+    {
+        public string Name { get; set; }
+        public Soundcategory Category { get; set; }
+        public string AudioFile { get; set; }
+    }
+}
